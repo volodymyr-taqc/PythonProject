4db1e3863a034ec391524ba5040361e63dc71c5c
Volodymyr <volodymyrgura97@gmail.com>
2019-06-17 19:08:37 +0300

    Add code with numders operators

--- PythonProject.py
+++ PythonProject.py
@@ -1,3 +1,9 @@
 # Hello
 name = input("Enter your name: ")
 print("Hello,", name)
+
+# Work with number operators
+number = 3 + 4 * 5 ** 2 + 7
+print(number)  # 110
+print(number % 12)
+print(number // 12)
